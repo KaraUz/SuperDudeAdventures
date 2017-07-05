b8962e4723184bd7d1d5eff46539480072f35547
Karolis_Uzdnavicius <uzda.kara@gmail.com>
2017-07-05 23:47:44 +0300

    Basic character movement (animated), basic camera.

---
+++ Assets/Scripts/CharacterScripts/CharacterController2D.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController2D : MonoBehaviour {
+    Rigidbody2D rb;
+    Animator anim;
+    SpriteRenderer spriteRenderer;
+
+    public GroundCheck groundCheck;
+    public float maxSpeedX = 1;
+    public float acceleration = 0.1f;
+    public float jumpPower = 1;
+
+    private float speed;
+    private bool facingRight = true;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void FixedUpdate()
+    {
+        HandleHorizontalSpeed();
+        HandleSwitchingDirections();
+
+        if (Input.GetButton("Jump") && groundCheck.StandingOn(Tags.GROUND))
+        {
+            rb.AddForce(Vector2.up * jumpPower);
+        }
+
+        anim.SetFloat("SpeedY", rb.velocity.y);
+        anim.SetFloat("SpeedX", speed);
+    }
+
+    private void HandleSwitchingDirections()
+    {
+        if (facingRight && speed < 0)
+        {
+            spriteRenderer.flipX = true;
+            facingRight = false;
+        }
+        else if (!facingRight && speed > 0)
+        {
+            spriteRenderer.flipX = false;
+            facingRight = true;
+        }
+    }
+    private void HandleHorizontalSpeed()
+    {
+        float horizontalAxis = Input.GetAxis("Horizontal");
+        if (horizontalAxis != 0)
+        {
+            speed = speed + acceleration * Mathf.Sign(horizontalAxis);
+            speed = Mathf.Abs(speed) > Mathf.Abs(maxSpeedX) ? maxSpeedX * Mathf.Sign(horizontalAxis) : speed;
+            rb.velocity = new Vector2(speed, groundCheck.StandingOn(Tags.GROUND) ? 0 : rb.velocity.y);
+        }
+        else
+        {
+            speed = rb.velocity.x;
+        }
+    }
+}
---
+++ Assets/Scripts/CharacterScripts/GroundCheck.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour {
+
+    private bool _grounded = false;
+    private List<string> _groundTags = new List<string>();
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        _grounded = true;
+        _groundTags.Add(other.tag);
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        _groundTags.Remove(other.tag);
+        if (_groundTags.Count == 0)
+            _grounded = false;
+    }
+
+    public bool StandingOn(string coliderTag)
+    {
+        return (_grounded == true && _groundTags.Contains(coliderTag));
+    }
+
+
+
+}
